99f56921a32385d601edc7106ea37636fe8c5ed6
Alejandro Linarez Rangel <alinarezrangel@gmail.com>
2024-02-11 12:11:36 -0500

    Versión preliminar del runtime en C.

---
+++ runtime/c/pdcrt.c
@@ -0,0 +1,435 @@
+#include <stdio.h>
+#include <limits.h>
+
+#include "pdcrt.h"
+
+
+static void pdcrt_error(pdcrt_ctx *ctx, const char* msj)
+{
+    ctx->mensaje_de_error = msj;
+    if(!ctx->hay_un_manejador_de_errores)
+    {
+        fprintf(stderr, "FATAL: %s", msj);
+        abort();
+    }
+    else
+    {
+        longjmp(ctx->manejador_de_errores, 1);
+    }
+}
+
+static void pdcrt_enomem(pdcrt_ctx *ctx)
+{
+    pdcrt_error(ctx, "Sin memoria");
+}
+
+
+static pdcrt_k pdcrt_recv_entero(pdcrt_ctx *ctx, int args, pdcrt_k k);
+static pdcrt_k pdcrt_recv_float(pdcrt_ctx *ctx, int args, pdcrt_k k);
+static pdcrt_k pdcrt_recv_booleano(pdcrt_ctx *ctx, int args, pdcrt_k k);
+static pdcrt_k pdcrt_recv_marco(pdcrt_ctx *ctx, int args, pdcrt_k k);
+
+#define pdcrt_objeto_entero(i) ((pdcrt_obj) { .recv = &pdcrt_recv_entero, .ival = (i) })
+#define pdcrt_objeto_float(f) ((pdcrt_obj) { .recv = &pdcrt_recv_float, .fval = (f) })
+#define pdcrt_objeto_booleano(b) ((pdcrt_obj) { .recv = &pdcrt_recv_booleano, .bval = (b) })
+#define pdcrt_objeto_marco(m) ((pdcrt_obj) { .recv = &pdcrt_recv_marco, .marco = (m) })
+
+
+static void pdcrt_gc_marcar(pdcrt_ctx *ctx, pdcrt_obj obj);
+static void pdcrt_recolectar_basura_simple(pdcrt_ctx *ctx);
+
+static void *pdcrt_alojar_obj(pdcrt_ctx *ctx, pdcrt_tipo_obj_gc tipo, size_t sz)
+{
+    if(ctx->cnt % 100 == 0)
+    {
+        pdcrt_recolectar_basura_simple(ctx);
+    }
+
+    pdcrt_cabecera_gc *h = malloc(sz);
+    h->generacion = ctx->gc.generacion;
+    h->tipo = tipo;
+    if(!ctx->gc.primero)
+        ctx->gc.primero = h;
+    h->siguiente = NULL;
+    h->anterior = ctx->gc.ultimo;
+    if(h->anterior)
+        h->anterior->siguiente = h;
+    ctx->gc.ultimo = h;
+    return h;
+}
+
+#define PDCRT_CABECERA_GC(v) ((pdcrt_cabecera_gc *) (v))
+
+static void pdcrt_gc_marcar_cabecera(pdcrt_ctx *ctx, pdcrt_cabecera_gc *h)
+{
+    switch(h->tipo)
+    {
+    case PDCRT_TGC_MARCO:
+    {
+        if(h->generacion == ctx->gc.generacion)
+            break;
+        h->generacion = ctx->gc.generacion;
+        pdcrt_marco *m = (pdcrt_marco *) h;
+        pdcrt_gc_marcar_cabecera(ctx, PDCRT_CABECERA_GC(m->k.marco));
+        for(size_t i = 0; i < (m->num_locales + m->num_capturas); i++)
+            pdcrt_gc_marcar(ctx, m->locales_y_capturas[i]);
+        break;
+    }
+    }
+}
+
+static pdcrt_tipo pdcrt_tipo_de_obj(pdcrt_obj o);
+
+static void pdcrt_gc_marcar(pdcrt_ctx *ctx, pdcrt_obj obj)
+{
+    pdcrt_cabecera_gc *h;
+    switch(pdcrt_tipo_de_obj(obj))
+    {
+    case PDCRT_TOBJ_ENTERO:
+    case PDCRT_TOBJ_FLOAT:
+    case PDCRT_TOBJ_BOOLEANO:
+        return;
+    case PDCRT_TOBJ_MARCO:
+        h = PDCRT_CABECERA_GC(obj.marco);
+        break;
+    }
+    pdcrt_gc_marcar_cabecera(ctx, h);
+}
+
+static void pdcrt_gc_marcar_todo(pdcrt_ctx *ctx)
+{
+    for(size_t i = 0; i < ctx->tam_pila; i++)
+        pdcrt_gc_marcar(ctx, ctx->pila[i]);
+    if(ctx->continuacion_actual.marco)
+    {
+        pdcrt_obj obj = pdcrt_objeto_marco(ctx->continuacion_actual.marco);
+        pdcrt_gc_marcar(ctx, obj);
+    }
+}
+
+static void pdcrt_gc_recolectar(pdcrt_ctx *ctx)
+{
+    for(pdcrt_cabecera_gc *h = ctx->gc.primero; h != NULL;)
+    {
+        pdcrt_cabecera_gc *s = h->siguiente;
+        if(h->generacion != ctx->gc.generacion)
+        {
+            if(h->anterior)
+                h->anterior->siguiente = h->siguiente;
+            if(h->siguiente)
+                h->siguiente->anterior = h->anterior;
+            if(h == ctx->gc.primero)
+                ctx->gc.primero = NULL;
+            if(h == ctx->gc.ultimo)
+                ctx->gc.ultimo = NULL;
+
+            free(h);
+        }
+        h = s;
+    }
+}
+
+static void pdcrt_recolectar_basura_simple(pdcrt_ctx *ctx)
+{
+    if(ctx->gc.generacion == INT_MAX)
+        ctx->gc.generacion = 0;
+    else
+        ctx->gc.generacion += 1;
+
+    pdcrt_gc_marcar_todo(ctx);
+    pdcrt_gc_recolectar(ctx);
+}
+
+
+static pdcrt_k pdcrt_recv_entero(pdcrt_ctx *ctx, int args, pdcrt_k k)
+{
+    // [yo, msj, ...#args]
+    assert(0 && "sin implementar");
+}
+
+static pdcrt_k pdcrt_recv_float(pdcrt_ctx *ctx, int args, pdcrt_k k)
+{
+    // [yo, msj, ...#args]
+    assert(0 && "sin implementar");
+}
+
+static pdcrt_k pdcrt_recv_booleano(pdcrt_ctx *ctx, int args, pdcrt_k k)
+{
+    // [yo, msj, ...#args]
+    assert(0 && "sin implementar");
+}
+
+static pdcrt_k pdcrt_recv_marco(pdcrt_ctx *ctx, int args, pdcrt_k k)
+{
+    // [yo, msj, ...#args]
+    assert(0 && "sin implementar");
+}
+
+static pdcrt_tipo pdcrt_tipo_de_obj(pdcrt_obj o)
+{
+    if(o.recv == &pdcrt_recv_entero)
+    {
+        return PDCRT_TOBJ_ENTERO;
+    }
+    else if(o.recv == &pdcrt_recv_float)
+    {
+        return PDCRT_TOBJ_FLOAT;
+    }
+    else if(o.recv == &pdcrt_recv_booleano)
+    {
+        return PDCRT_TOBJ_BOOLEANO;
+    }
+    else if(o.recv == &pdcrt_recv_marco)
+    {
+        return PDCRT_TOBJ_MARCO;
+    }
+    else
+    {
+        assert(0 && "inalcanzable");
+    }
+}
+
+
+pdcrt_marco* pdcrt_crear_marco(pdcrt_ctx *ctx, size_t locales, size_t capturas, int args, pdcrt_k k)
+{
+    pdcrt_marco *m = pdcrt_alojar_obj(ctx, PDCRT_TGC_MARCO, sizeof(pdcrt_marco) + sizeof(pdcrt_obj) * (locales + capturas));
+    if(!m)
+        pdcrt_enomem(ctx);
+    m->args = args;
+    m->k = k;
+    m->num_locales = locales;
+    m->num_capturas = capturas;
+    size_t top = ctx->tam_pila;
+    for(size_t i = 0; i < capturas; i++)
+    {
+        m->locales_y_capturas[locales + i] = ctx->pila[(top - capturas) + i];
+    }
+    ctx->tam_pila -= capturas;
+    return m;
+}
+
+
+pdcrt_ctx *pdcrt_crear_contexto(void)
+{
+    pdcrt_ctx *ctx = malloc(sizeof(pdcrt_ctx));
+    ctx->tam_pila = 0;
+    ctx->pila = NULL;
+    ctx->cap_pila = 0;
+    ctx->gc.primero = ctx->gc.ultimo = NULL;
+    ctx->gc.generacion = 0;
+    ctx->cnt = 0;
+    volatile int x;
+    ctx->inicio_del_stack = (uintptr_t) &x;
+    ctx->tam_stack = 1 * 1024 * 1024; // 1 MiB
+    ctx->mensaje_de_error = NULL;
+    ctx->hay_un_manejador_de_errores = false;
+    ctx->hay_una_salida_del_trampolin = false;
+    ctx->continuacion_actual = (pdcrt_k) { .kf = NULL, .marco = NULL };
+    return ctx;
+}
+
+void pdcrt_cerrar_contexto(pdcrt_ctx *ctx)
+{
+    for(pdcrt_cabecera_gc *h = ctx->gc.primero; h != NULL;)
+    {
+        pdcrt_cabecera_gc *s = h->siguiente;
+        free(h);
+        h = s;
+    }
+
+    free(ctx->pila);
+    free(ctx);
+}
+
+static void pdcrt_extender_pila(pdcrt_ctx *ctx, size_t num_elem)
+{
+    if((num_elem + ctx->tam_pila) > ctx->cap_pila)
+    {
+        size_t nueva_cap = num_elem + ctx->tam_pila;
+        void *nueva_pila = realloc(ctx->pila, sizeof(pdcrt_obj) * nueva_cap);
+        if(!nueva_pila)
+            pdcrt_enomem(ctx);
+        ctx->pila = nueva_pila;
+        ctx->cap_pila = nueva_cap;
+    }
+}
+
+#define pdcrt_empujar(ctx, v) (ctx)->pila[(ctx)->tam_pila++] = (v)
+#define pdcrt_sacar(ctx) (ctx)->pila[--(ctx)->tam_pila]
+
+void pdcrt_empujar_entero(pdcrt_ctx *ctx, pdcrt_entero i)
+{
+    pdcrt_extender_pila(ctx, 1);
+    pdcrt_empujar(ctx, pdcrt_objeto_entero(i));
+}
+
+void pdcrt_empujar_float(pdcrt_ctx *ctx, pdcrt_float f)
+{
+    pdcrt_extender_pila(ctx, 1);
+    pdcrt_empujar(ctx, pdcrt_objeto_float(f));
+}
+
+void pdcrt_empujar_espacio_de_nombres(pdcrt_ctx *ctx)
+{
+    // TODO
+    pdcrt_extender_pila(ctx, 1);
+    pdcrt_empujar(ctx, pdcrt_objeto_entero(1));
+}
+
+bool pdcrt_saltar_condicional(pdcrt_ctx *ctx)
+{
+    pdcrt_obj o = pdcrt_sacar(ctx);
+    if(pdcrt_tipo_de_obj(o) != PDCRT_TOBJ_BOOLEANO)
+    {
+        pdcrt_error(ctx, "Se esperaba un objeto de tipo Booleano");
+    }
+    return o.bval;
+}
+
+pdcrt_k pdcrt_saltar(pdcrt_ctx *ctx, pdcrt_marco *m, pdcrt_kf kf)
+{
+    return (pdcrt_k) {
+        .kf = kf,
+        .marco = m,
+    };
+}
+
+pdcrt_k pdcrt_enviar_mensaje(pdcrt_ctx *ctx, pdcrt_marco *m,
+                             const char* msj,
+                             const int* proto, size_t nproto,
+                             pdcrt_kf kf)
+{
+    pdcrt_extender_pila(ctx, 1);
+    pdcrt_k k = {
+        .kf = kf,
+        .marco = m,
+    };
+    for(ssize_t i = nproto - 1; i >= 0; i++)
+    {
+        assert(proto[i] == 0);
+        ctx->pila[ctx->tam_pila - i] = ctx->pila[(ctx->tam_pila - i) - 1];
+    }
+    ctx->pila[((ctx->tam_pila++) - nproto) - 1] = pdcrt_objeto_entero(-1);
+    pdcrt_obj t = ctx->pila[(ctx->tam_pila - nproto) - 2];
+    return (*t.recv)(ctx, nproto, k);
+}
+
+pdcrt_k pdcrt_prn(pdcrt_ctx *ctx, pdcrt_marco *m, pdcrt_kf kf)
+{
+    pdcrt_obj o = pdcrt_sacar(ctx);
+    printf("%p %ld %f", (void*) o.recv, o.ival, o.fval);
+    return (pdcrt_k) {
+        .kf = kf,
+        .marco = m,
+    };
+}
+
+void pdcrt_prnl(pdcrt_ctx *ctx)
+{
+    (void) ctx;
+    puts("");
+}
+
+pdcrt_k pdcrt_devolver(pdcrt_ctx *ctx, pdcrt_marco *m, int rets)
+{
+    (void) ctx;
+    assert(rets == 1);
+    return m->k;
+}
+
+pdcrt_k pdcrt_exportar(pdcrt_ctx *ctx, pdcrt_marco *m)
+{
+    // TODO
+    (void) ctx;
+    return m->k;
+}
+
+bool pdcrt_stack_lleno(pdcrt_ctx *ctx)
+{
+    volatile int x;
+    uintptr_t xp = (uintptr_t) &x;
+    size_t diff;
+    if(xp > ctx->inicio_del_stack)
+        diff = xp - ctx->inicio_del_stack;
+    else
+        diff = ctx->inicio_del_stack - xp;
+    return diff > ctx->tam_stack;
+}
+
+static pdcrt_k pdcrt_continuacion_de_ejecutar(pdcrt_ctx *ctx, pdcrt_marco *m)
+{
+    assert(ctx->hay_una_salida_del_trampolin);
+    longjmp(ctx->salir_del_trampolin, 1);
+}
+
+static bool pdcrt_ejecutar_opt(pdcrt_ctx *ctxp, int args, pdcrt_f f, bool protegido)
+{
+    pdcrt_ctx * volatile ctx = ctxp;
+    pdcrt_marco *m = pdcrt_crear_marco(ctx, 0, 0, 0, (pdcrt_k){0});
+    pdcrt_k k = {
+        .kf = &pdcrt_continuacion_de_ejecutar,
+        .marco = m,
+    };
+    volatile size_t tam_pila = ctx->tam_pila;
+    volatile jmp_buf viejo_s;
+    volatile jmp_buf viejo_e;
+    memcpy((jmp_buf *) &viejo_s, &ctx->salir_del_trampolin, sizeof(jmp_buf));
+    memcpy((jmp_buf *) &viejo_e, &ctx->manejador_de_errores, sizeof(jmp_buf));
+    volatile bool habia_s = ctx->hay_una_salida_del_trampolin;
+    volatile bool habia_e = ctx->hay_un_manejador_de_errores;
+
+#define CLEANUP()                                                       \
+    do                                                                  \
+    {                                                                   \
+        memcpy(&ctx->salir_del_trampolin, (jmp_buf *) &viejo_s, sizeof(jmp_buf)); \
+        memcpy(&ctx->manejador_de_errores, (jmp_buf *) &viejo_e, sizeof(jmp_buf)); \
+        ctx->hay_una_salida_del_trampolin = habia_s;                    \
+        ctx->hay_un_manejador_de_errores = habia_e;                     \
+    }                                                                   \
+    while(0)
+
+    if(setjmp(ctx->salir_del_trampolin) > 0)
+    {
+        CLEANUP();
+        return true;
+    }
+    if(protegido)
+    {
+        if(setjmp(ctx->manejador_de_errores) > 0)
+        {
+            CLEANUP();
+            ctx->tam_pila = tam_pila; // Saca los valores intermediarios
+            return false;
+        }
+
+        ctx->hay_un_manejador_de_errores = true;
+    }
+
+    ctx->hay_una_salida_del_trampolin = true;
+    ctx->continuacion_actual = f(ctx, args, k);
+    do
+    {
+        ctx->continuacion_actual = (*ctx->continuacion_actual.kf)(ctx, ctx->continuacion_actual.marco);
+    }
+    while(1);
+}
+
+void pdcrt_ejecutar(pdcrt_ctx *ctx, int args, pdcrt_f f)
+{
+    pdcrt_ejecutar_opt(ctx, args, f, false);
+}
+
+bool pdcrt_ejecutar_protegido(pdcrt_ctx *ctx, int args, pdcrt_f f)
+{
+    return pdcrt_ejecutar_opt(ctx, args, f, true);
+}
+
+int pdcrt_main(int argc, char **argv, pdcrt_f f)
+{
+    (void) argc;
+    (void) argv;
+    pdcrt_ctx *ctx = pdcrt_crear_contexto();
+    pdcrt_ejecutar(ctx, 0, f);
+    pdcrt_cerrar_contexto(ctx);
+    return 0;
+}
---
+++ runtime/c/pdcrt.h
@@ -0,0 +1,152 @@
+#ifndef PDCRT_H
+#define PDCRT_H
+
+#include <setjmp.h>
+#include <assert.h>
+#include <string.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+typedef int64_t pdcrt_entero;
+typedef double pdcrt_float;
+
+
+struct pdcrt_ctx;
+typedef struct pdcrt_ctx pdcrt_ctx;
+
+
+struct pdcrt_marco;
+typedef struct pdcrt_marco pdcrt_marco;
+
+struct pdcrt_k;
+typedef struct pdcrt_k pdcrt_k;
+
+typedef struct pdcrt_k (*pdcrt_f)(pdcrt_ctx *, int args, struct pdcrt_k);
+typedef struct pdcrt_k (*pdcrt_kf)(pdcrt_ctx *, pdcrt_marco *);
+
+struct pdcrt_k
+{
+    pdcrt_kf kf;
+    pdcrt_marco *marco;
+};
+
+
+typedef enum pdcrt_tipo_obj_gc
+{
+    PDCRT_TGC_MARCO,
+} pdcrt_tipo_obj_gc;
+
+typedef struct pdcrt_cabecera_gc
+{
+    struct pdcrt_cabecera_gc *siguiente, *anterior;
+    int generacion;
+    pdcrt_tipo_obj_gc tipo;
+} pdcrt_cabecera_gc;
+
+typedef struct pdcrt_gc
+{
+    pdcrt_cabecera_gc *primero, *ultimo;
+    int generacion;
+} pdcrt_gc;
+
+
+typedef struct pdcrt_obj
+{
+    pdcrt_f recv;
+    union
+    {
+        pdcrt_entero ival;
+        pdcrt_float fval;
+        bool bval;
+        pdcrt_marco *marco;
+    };
+} pdcrt_obj;
+
+typedef enum pdcrt_tipo
+{
+    PDCRT_TOBJ_ENTERO,
+    PDCRT_TOBJ_FLOAT,
+    PDCRT_TOBJ_BOOLEANO,
+    PDCRT_TOBJ_MARCO,
+} pdcrt_tipo;
+
+
+struct pdcrt_marco
+{
+    pdcrt_cabecera_gc gc;
+    int args;
+    pdcrt_k k;
+    size_t num_locales;
+    size_t num_capturas;
+    pdcrt_obj locales_y_capturas[];
+};
+
+pdcrt_marco* pdcrt_crear_marco(pdcrt_ctx *ctx, size_t locales, size_t capturas, int args, pdcrt_k k);
+
+
+struct pdcrt_ctx
+{
+    pdcrt_obj* pila;
+    size_t tam_pila;
+    size_t cap_pila;
+
+    pdcrt_gc gc;
+    pdcrt_k continuacion_actual;
+
+    unsigned int cnt;
+
+    uintptr_t inicio_del_stack;
+    size_t tam_stack;
+
+    const char* mensaje_de_error;
+    jmp_buf manejador_de_errores;
+    bool hay_un_manejador_de_errores;
+    jmp_buf salir_del_trampolin;
+    bool hay_una_salida_del_trampolin;
+};
+
+pdcrt_ctx *pdcrt_crear_contexto(void);
+void pdcrt_cerrar_contexto(pdcrt_ctx *ctx);
+
+void pdcrt_empujar_entero(pdcrt_ctx *ctx, pdcrt_entero i);
+void pdcrt_empujar_float(pdcrt_ctx *ctx, pdcrt_float f);
+void pdcrt_empujar_espacio_de_nombres(pdcrt_ctx *ctx);
+
+void pdcrt_ejecutar(pdcrt_ctx *ctx, int args, pdcrt_f f);
+bool pdcrt_ejecutar_protegido(pdcrt_ctx *ctx, int args, pdcrt_f f);
+
+#ifdef PDCRT_INTERNO
+
+bool pdcrt_saltar_condicional(pdcrt_ctx *ctx);
+pdcrt_k pdcrt_saltar(pdcrt_ctx *ctx, pdcrt_marco *m, pdcrt_kf kf);
+pdcrt_k pdcrt_enviar_mensaje(pdcrt_ctx *ctx, pdcrt_marco *m,
+                             const char* msj,
+                             const int* proto, size_t nproto,
+                             pdcrt_kf kf);
+pdcrt_k pdcrt_prn(pdcrt_ctx *ctx, pdcrt_marco *m, pdcrt_kf kf);
+void pdcrt_prnl(pdcrt_ctx *ctx);
+pdcrt_k pdcrt_devolver(pdcrt_ctx *ctx, pdcrt_marco *m, int rets);
+pdcrt_k pdcrt_exportar(pdcrt_ctx *ctx, pdcrt_marco *m);
+
+bool pdcrt_stack_lleno(pdcrt_ctx *ctx);
+
+int pdcrt_main(int argc, char **argv, pdcrt_f f);
+
+#define PDCRT_DECLARAR_ENTRYPOINT(mod, fmain)                       \
+    pdcrt_k pdc_instalar_##mod(pdcrt_ctx *ctx, int args, pdcrt_k k) \
+    {                                                               \
+        return fmain(ctx, args, k);                                 \
+    }
+
+#define PDCRT_DECLARAR_MAIN(mod)                            \
+    int main(int argc, char **argv)                         \
+    {                                                       \
+        return pdcrt_main(argc, argv, &pdc_instalar_##mod); \
+    }
+
+#endif  /* PDCRT_INTERNO */
+
+#endif /* PDCRT_H */
